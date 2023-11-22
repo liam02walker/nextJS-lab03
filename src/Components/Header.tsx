28fbdc58cea7534372828071d1c203ea2f749ec8
liam02walker <17liamwalker02@gmail.com>
2023-11-22 09:50:47 +0000

    all css styles done, lab 5 done or 6

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <div>
-      <h1 className="flex flex-col items-center p-8 text-2xl font-bold">ABOUT ME</h1>
+    <div className={`${styles.headerStyle}`}>
+      <h1>ABOUT ME</h1>
+      <div className={`${styles.linkContainer}`}>
+        <Link className={`${styles.linkStyle}`} href="/about">
+          ABOUT
+        </Link>
+        <Link className={`${styles.linkStyle}`} href="/games">
+          GAMES
+        </Link>
+        <Link className={`${styles.linkStyle}`} href="/pets">
+          PETS
+        </Link>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-1 items-center justify-between flex-col">
-      <div className="p-[16px] rounded-xl max-w-[60%] max-h-[80%] flex flex-col justify-center items-center bg-white text-black font-bold">
+      <div className="m-[8px] p-[16px] rounded-xl max-w-[40%] max-h-[80%] flex flex-col justify-center items-center bg-white text-black font-bold">
         <h2 className="text-lg pb-4">Liam Walker</h2>
         <p className="pb-2">My name is Liam Walker, and I am learning Tailwind for the first time (send me some help please...)</p>
         <p className="pb-2">Tailwind is probably useful, but I simply don't like it.</p>
         <p className="pb-2">I want my old CSS back</p>
-        <Link href="/about">Click here to view more</Link>
-        <Link href="/games">Click here to view games</Link>
-        <Link href="/pets">Click here to view pets</Link>
       </div>
     </main>
   );
